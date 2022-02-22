8ba2f953fe976754847c6123e210305cf3f2d358
divyanshudp1995 <97895205+divyanshudp1995@users.noreply.github.com>
2022-02-22 15:20:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,37 +42,3 @@ elif page == "Japan":
 elif page == "China":
     print(show_predict_page())
     
-
-gender_1 = {
-"Male",
-"Female",
-}
-
-marital_status = {
-"Yes",
-"No",
-}
-
-education = {
-"Graduate",
-"Not Graduate",
-}
-
-credit_history = {
-"1",
-"0",
-}
-
-
-
-gender = st.selectbox("Gender",gender_1)
-marital_status = st.selectbox("Marital Status",marital_status)
-education = st.selectbox("Education",education)
-credit = st.selectbox("Credit Score",credit_history)
-income = st.slider("Income (in $)", 0, 100000, 1000,100)
-loan_amount = st.slider("Loan Amount (in $)", 0, 1000, 100,10)
-loan_amount_term = st.slider("Loan Amount Term (in months)", 0, 480, 40,20)
-
-ok = st.button("Calculate Loan Status")
-if ok:
-  st.subheader(f"The Loan Approval Status  - " + " ")
