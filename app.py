3348fe4ac121ae413c5e8b2209069d7a649824c7
divyanshudp1995 <97895205+divyanshudp1995@users.noreply.github.com>
2022-02-22 14:37:31 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pickle
+
+st.set_option('deprecation.showfileUploaderEncoding',False) 
+model = pickle.load(open('new_model.pkl','rb'))
+
+gender = st.selectbox("Gender",gender_1)
+marital_status = st.selectbox("Marital Status",marital_status)
+education = st.selectbox("Education",education)
+credit = st.selectbox("Credit Score",credit_history)
+income = st.slider("Income (in $)", 0, 100000, 1000,100)
+loan_amount = st.slider("Loan Amount (in $)", 0, 1000, 100,10)
+loan_amount_term = st.slider("Loan Amount Term (in months)", 0, 480, 40,20)
